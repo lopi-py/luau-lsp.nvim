60e33a9ce2ee348a377fda61366e96513bd97643
lopy <70210066+lopi-py@users.noreply.github.com>
2024-01-04 18:44:42 -0500

    feat: add log to file

--- lua/luau-lsp/log.lua
+++ lua/luau-lsp/log.lua
@@ -1,13 +1,29 @@
+local Path = require "plenary.path"
+local util = require "luau-lsp.util"
+
+local PLUGIN_NAME = "luau-lsp.nvim"
+
+local levels = vim.deepcopy(vim.log.levels)
+vim.tbl_add_reverse_lookup(levels)
+
+local function create_logger(level)
+  return function(message, ...)
+    local file = Path:new(util.storage_file "luau-lsp.log")
+    local timestr = vim.fn.strftime "%H:%M:%S"
+    message = string.format(message, ...)
+
+    file:write(string.format("%s[%s]: %s\n", timestr, levels[level], message), "a")
+
+    if level >= levels.WARN then
+      vim.notify(message, level, { title = PLUGIN_NAME })
+    end
+  end
+end
+
 local M = {}
 
-local plugin_name = "luau-lsp.nvim"
-
-function M.info(msg, ...)
-  vim.notify(msg:format(...), vim.lsp.log_levels.INFO, { title = plugin_name })
-end
-
-function M.error(msg, ...)
-  vim.notify(msg:format(...), vim.lsp.log_levels.ERROR, { title = plugin_name })
-end
+M.info = create_logger(levels.INFO)
+M.warn = create_logger(levels.WARN)
+M.error = create_logger(levels.ERROR)
 
 return M
