fd9a2ee74acebcccd6a54b831cc9230605bed6b3
lopy <70210066+lopi-py@users.noreply.github.com>
2024-01-05 15:35:34 -0500

    chore: remove unused code

--- lua/luau-lsp/types/roblox.lua
+++
@@ -1,4 +0,0 @@
-return {
-  types = "https://raw.githubusercontent.com/JohnnyMorganz/luau-lsp/main/scripts/globalTypes.d.lua",
-  docs = "https://raw.githubusercontent.com/MaximumADHD/Roblox-Client-Tracker/roblox/api-docs/en-us.json",
-}
