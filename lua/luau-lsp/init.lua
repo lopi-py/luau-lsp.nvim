71aa0eb09ceb7ea1f37734a32c2e7e983da32cd1
lopy <70210066+lopi-py@users.noreply.github.com>
2024-07-29 23:38:48 -0500

    refactor!: remove .treesitter() function

--- lua/luau-lsp/init.lua
+++ lua/luau-lsp/init.lua
@@ -1,10 +1,5 @@
 local M = {}
 
-function M.treesitter()
-  local log = require "luau-lsp.log"
-  log.warn "A custom luau treesitter parser is no longer required"
-end
-
 ---@param opts luau-lsp.Config
 function M.config(opts)
   require("luau-lsp.config").config(opts)
